b0b9028d820b7125ff7479b13ea8f8f7f053d01e
Bruno Escalona Ruiz <bruno.escalona@scrm.lidl>
2024-01-24 08:17:33 +0100

    Remove unnecessary material and material3 dependencies

--- SpannedToAnnotatedString/build.gradle.kts
+++ SpannedToAnnotatedString/build.gradle.kts
@@ -42,12 +42,11 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
